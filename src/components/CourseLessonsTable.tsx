16f87dd40f1a4333042371a5febadfee88669b04
John Doe <johndoe@example.com>
2025-09-05 16:11:04 +0630

    add line p tag in CourseLessonTable

--- src/components/CourseLessonsTable.tsx
+++ src/components/CourseLessonsTable.tsx
@@ -7,8 +7,10 @@ export default function CourseLessonsTable() {
     const [courseLessons, setCourseLessons] = useState<CourseLessonDto[]>([]);
   return (
     <div>
-        
+        <h1>Course Lessons</h1>
         <h2>Course Lessons - CourseId : {id}</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione explicabo! Sint quia quam atque tempora nam sapiente recusandae molestias. Quae illo accusantium asperiores itaque, minima hic quaerat atque nam?</p>
 
     </div>
   )
